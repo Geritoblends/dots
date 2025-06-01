238eda9995d0a2cd7be8137fdb3a042b6cbc4458
Gero <geronimosotelolopez@gmail.com>
2025-06-01 00:50:07 -0600

    Update dotfiles on 2025-06-01 00:50

--- .bashrc
+++ .bashrc
@@ -3,12 +3,15 @@
 #
 
 # If not running interactively, don't do anything
+eval "$(starship init bash)"
 [[ $- != *i* ]] && return
+
+export MAMBA_ROOT_PREFIX=/mnt/hdd/micromamba
+
+export PATH="$HOME/.local/bin:$PATH"
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-PS1='\n  \W ❯ '
-
 alias x="poweroff"
 alias hyprconf="nvim -n ~/.config/hypr/hyprland.conf"
 alias waybarconf="nvim -n ~/.config/waybar/config"
@@ -17,8 +20,21 @@ alias mm="micromamba"
 alias musictui="ncmpcpp"
 alias nvimconf="nvim -n ~/.config/nvim/init.lua"
 alias snvim="sudo -E nvim"
-
-export MAMBA_ROOT_PREFIX=/mnt/hdd/micromamba
-
-export PATH="$HOME/.local/bin:$PATH"
-
+alias bedit="nvim -n ~/.bashrc"
+alias bsource="source ~/.bashrc"
+alias footconf="nvim -n ~/.config/foot/foot.ini"
+alias projects="cd ~/Documents/projects"
+alias starshipconf="nvim -n ~/.config/starship.toml"
+alias woficonf="nvim -n ~/.config/wofi/config"
+alias woficss="nvim -n ~/.config/wofi/style.css"
+alias anyrunconf="nvim -n ~/.config/anyrun/config.ron"
+alias anyruncss="nvim -n ~/.config/anyrun/style.css"
+alias hyprlockconf="nvim -n ~/.config/hypr/hyprlock.conf"
+alias off="poweroff"
+alias cpustats="auto-cpufreq --stats"
+alias pact='source $(poetry env info --path)/bin/activate'
+alias deact="deactivate"
+alias songs="ncmpcpp"
+alias ppy="poetry run python"
+alias jenny="cd /home/gero/Documents/servers/jenny/ && ./run.sh"
+alias neofetch="fastfetch --logo-padding-top 7"
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,88 +1,396 @@
--- init.lua
-
--- Set up basic options
+-- Basic settings
 vim.g.mapleader = " "
-vim.o.number = true          -- Show line numbers
-vim.o.relativenumber = true  -- Relative line numbers
-vim.o.tabstop = 4            -- Set tab width to 4 spaces
-vim.o.shiftwidth = 4         -- Set shift width to 4 spaces
-vim.o.expandtab = true       -- Use spaces instead of tabs
-vim.o.hlsearch = true        -- Highlight search results
-vim.o.incsearch = true       -- Incremental search
-
--- Plugin manager setup (Lazy.nvim)
--- Install Lazy.nvim if not already installed
-local lazypath = vim.fn.stdpath("data") .. "/site/pack/packer/start/lazy.nvim"
+vim.g.maplocalleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.mouse = 'a'
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Configure plugins using Lazy.nvim
+-- Plugin setup with lazy.nvim
 require("lazy").setup({
-  -- Plugins go here
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup {}
-      vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
-    end,
-  }, 
-  {
-    "nvim-treesitter/nvim-treesitter",
-  },
-  {
-    "williamboman/mason.nvim",
-  },
-  {
-    "williamboman/mason-lspconfig.nvim"
-  },
-  {
-    "neovim/nvim-lspconfig"
-  },
+    {
+        "Exafunction/windsurf.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            require("codeium").setup({
+                virtual_text = {
+                    enabled = true,
+                    manual = false,
+                    key_bindings = {
+                        accept = "<M-i>",
+                        accept_word = false,
+                        accept_line = false,
+                        next = "<M-j>",
+                        prev = "<M-k>",
+                        dismiss = "<M-u>",
+                    },
+                },
 
-{
-    'nvim-tree/nvim-tree.lua',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      -- You can configure nvim-tree here
-      require("nvim-tree").setup {}
 
-      -- Keybinding to toggle NvimTree
-      vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-    end,
-    keys = { "<leader>e" }
-},
-  {
-    "EdenEast/nightfox.nvim"
-  },
+            })
+        end
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- hey
+        }
+    },
+    {
+        'saghen/blink.cmp',
+        dependencies = {
+            {
+                'rafamadriz/friendly-snippets'
+            },
+        },
+        version = 'v0.*',
+        opts = {
+            keymap = { preset = 'default' },
 
-  {
-    'L3MON4D3/LuaSnip',
-    dependencies = { 'rafamadriz/friendly-snippets' },
-    config = function()
-    require'luasnip.loaders.from_vscode'.lazy_load()
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = 'mono'
+            },
+            signature = { enabled = true }
+        },
+    },
+
+    -- nvim-tree file explorer
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons", -- For file icons
+        },
+        config = function()
+            -- Disable netrw (recommended by nvim-tree)
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+
+            require("nvim-tree").setup({
+                sort_by = "case_sensitive",
+                view = {
+                    width = 30,
+                },
+                renderer = {
+                    group_empty = true,
+                    icons = {
+                        show = {
+                            file = true,
+                            folder = true,
+                            folder_arrow = true,
+                            git = true,
+                        },
+                    },
+                    highlight_git = true,
+                    add_trailing = false,
+                },
+                filters = {
+                    dotfiles = false,
+                },
+                git = {
+                    enable = true,
+                    ignore = false,
+                },
+            })
+
+            -- Set up gruvbox colors for nvim-tree
+            vim.api.nvim_create_autocmd("ColorScheme", {
+                pattern = "*",
+                callback = function()
+                    -- Customize these colors to match gruvbox
+                    vim.api.nvim_set_hl(0, "NvimTreeFolderName", { fg = "#d5c4a1" })
+                    vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { fg = "#fabd2f" })
+                    vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { fg = "#ebdbb2" })
+                    vim.api.nvim_set_hl(0, "NvimTreeIndentMarker", { fg = "#665c54" })
+                    vim.api.nvim_set_hl(0, "NvimTreeGitDirty", { fg = "#fe8019" })
+                    vim.api.nvim_set_hl(0, "NvimTreeGitNew", { fg = "#b8bb26" })
+                    vim.api.nvim_set_hl(0, "NvimTreeGitDeleted", { fg = "#fb4934" })
+                    vim.api.nvim_set_hl(0, "NvimTreeSpecialFile", { fg = "#83a598" })
+                    vim.api.nvim_set_hl(0, "NvimTreeExecFile", { fg = "#b8bb26" })
+                    vim.api.nvim_set_hl(0, "NvimTreeImageFile", { fg = "#d3869b" })
+                end,
+            })
+
+            -- Key mappings for nvim-tree
+            vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { silent = true })
+            vim.keymap.set('n', '<leader>e', ':NvimTreeFocus<CR>', { silent = true })
+        end,
+    },
+
+    -- Essential plugins
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = function()
+            vim.cmd("TSUpdate")
+        end,
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "vim", "bash", "markdown", "python", "javascript", "typescript", "tsx", "html", "css", "markdown", "markdown_inline", "json" },
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local telescope = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
+            vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
+        end,
+    },
+
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "pyright" },
+                automatic_installation = true,
+            })
+
+            -- Basic LSP setup
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup {}
+            lspconfig.pyright.setup {}
+            -- LSP keybindings
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
+
+    {
+        'windwp/nvim-ts-autotag',
+        config = function()
+            require('nvim-ts-autotag').setup()
+        end,
+    },
+
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                timeout_ms = 500,
+            },
+        },
+    },
+    {
+
+        "sphamba/smear-cursor.nvim",
+
+        opts = {
+            -- Smear cursor when switching buffers or windows.
+            smear_between_buffers = true,
+
+            -- Smear cursor when moving within line or to neighbor lines.
+            -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+            smear_between_neighbor_lines = true,
+
+            -- Draw the smear in buffer space instead of screen space when scrolling
+            scroll_buffer_space = true,
+
+            -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+            -- Smears will blend better on all backgrounds.
+            legacy_computing_symbols_support = false,
+
+            -- Smear cursor in insert mode.
+            -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+            smear_insert_mode = true,
+        }
+    },
+    -- lazy.nvim
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim"
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+        }
+    },
+    {
+        "karb94/neoscroll.nvim",
+        opts = {},
+    },
+    {
+        "goolord/alpha-nvim",
+        -- dependencies = { 'echasnovski/mini.icons' },
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            local startify = require("alpha.themes.startify")
+            -- available: devicons, mini, default is mini
+            -- if provider not loaded and enabled is true, it will try to use another provider
+            startify.file_icons.provider = "devicons"
+            require("alpha").setup(
+                startify.config
+            )
+        end,
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+})
+require("colors.oxocabron").setup()
+
+-- In your lualine config file (e.g., lua/plugins/lualine.lua)
+require('lualine').setup({
+    options = {
+        theme = {
+            normal = {
+                a = { fg = '#f2f4f8', bg = 'NONE' }, -- Remove bg for active mode
+                b = { fg = '#f2f4f8', bg = 'NONE' },
+                c = { fg = '#f2f4f8', bg = 'NONE' }
+            },
+            insert = {
+                a = { fg = '#25be6a', bg = 'NONE' }, -- Your green from palette
+                b = { fg = '#f2f4f8', bg = 'NONE' },
+                c = { fg = '#f2f4f8', bg = 'NONE' }
+            },
+            visual = {
+                a = { fg = '#be95ff', bg = 'NONE' }, -- Your magenta from palette
+                b = { fg = '#f2f4f8', bg = 'NONE' },
+                c = { fg = '#f2f4f8', bg = 'NONE' }
+            },
+            replace = {
+                a = { fg = '#ee5396', bg = 'NONE' }, -- Your red from palette
+                b = { fg = '#f2f4f8', bg = 'NONE' },
+                c = { fg = '#f2f4f8', bg = 'NONE' }
+            },
+            command = {
+                a = { fg = '#78a9ff', bg = 'NONE' }, -- Your blue from palette
+                b = { fg = '#f2f4f8', bg = 'NONE' },
+                c = { fg = '#f2f4f8', bg = 'NONE' }
+            },
+            inactive = {
+                a = { fg = '#525252', bg = 'NONE' }, -- Your bright_black
+                b = { fg = '#525252', bg = 'NONE' },
+                c = { fg = '#525252', bg = 'NONE' }
+            }
+        },
+        component_separators = { left = '│', right = '│' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
+})
+-- Basic keymaps
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Add this to the end of your init.lua file
+
+-- Make terminal split always appear at the bottom
+vim.api.nvim_create_autocmd("TermOpen", {
+    pattern = "*",
+    callback = function()
+        -- Move the terminal to the bottom
+        vim.cmd("wincmd J")
+        -- Enter insert mode automatically (optional)
+        vim.cmd("startinsert")
     end,
-  },
 })
 
-vim.opt.clipboard = "unnamedplus"
-vim.o.laststatus = 0
+-- Create a custom command for convenience (optional)
+vim.api.nvim_create_user_command("Terminal", function()
+    vim.cmd("split")
+    vim.cmd("terminal")
+end, {})
 
--- Set up colorscheme
-vim.cmd([[
-    colorscheme carbonfox
-    highlight Normal guibg=#000000
-    highlight NormalNC guibg=#000000
-]])
-
-require("mason").setup()
+-- Optional keymap for quickly opening a terminal at the bottom
+vim.keymap.set("n", "<leader>t", ":split | terminal<CR>", { silent = true })
 
 
+-- Add this to your init.lua to make <Esc> exit terminal mode
+vim.api.nvim_create_autocmd("TermOpen", {
+    pattern = "*",
+    callback = function()
+        -- Local terminal mapping for <Esc>
+        vim.api.nvim_buf_set_keymap(0, 't', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+    end,
+})
